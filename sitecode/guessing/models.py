652076243b04936650e958f4e7cef614fbf9ed10
Roosa <roosa_tammela@hotmail.com>
2014-11-27 16:34:19 +0000

    implemented basic models Matchlist, Matchselect, Mathcchoice

--- sitecode/guessing/models.py
+++ sitecode/guessing/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Matchlist(models.Model):
+	match_list = models.CharField(max_length=200)
+	
+class Matchselect(models.Model):
+	match_select = models.ForeignKey(Matchlist)
+	match_info = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('data published')
+	
+class Matchchoice(models.Model):
+	# Match choice must come from the match choice from Matchselect
+	match = models.ForeignKey(Matchselect)
+	winner_choice = models.BooleanField(RadioSelect)
+	votes = models.IntegerField(default=0)
