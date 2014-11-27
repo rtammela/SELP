1501d8aa74033342eff94b31fdc6e31f31d6e790
Roosa <roosa_tammela@hotmail.com>
2014-11-27 17:15:04 +0000

    added __str__() methods to classes in guessing module

--- sitecode/guessing/models.py
+++ sitecode/guessing/models.py
@@ -4,14 +4,20 @@ from django.db import models
 
 class Matchlist(models.Model):
 	match_list = models.CharField(max_length=200)
+	def __str__(self):
+		return self.match_list
 	
 class Matchselect(models.Model):
 	match_select = models.ForeignKey(Matchlist)
 	match_info = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('data published')
+	def __str__(self):
+		return self.match_info
 	
 class Matchchoice(models.Model):
 	# Match choice must come from the match choice from Matchselect
 	match = models.ForeignKey(Matchselect)
 	winner_choice = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.winner_choice
