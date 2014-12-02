0ec2ca17fb78f981bd17ff529e3beedf51888b4d
Roosa <roosa_tammela@hotmail.com>
2014-12-02 18:23:28 +0000

    created placeholder views for 'details', 'results', and 'vote' for matches based on match id

--- sitecode/guessing/urls.py
+++ sitecode/guessing/urls.py
@@ -4,4 +4,7 @@ from guessing import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<matchselect_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<matchselect_id>\d+)/results/$', views.results, name='results'),
+	url(r'^(?P<matchselect_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- sitecode/guessing/views.py
+++ sitecode/guessing/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello world~")
+	
+def detail(request, matchselect_id):
+	return HttpResponse("Selected match: %s." % matchselect_id)
+	
+def results(request, matchselect_id):
+	response = "Results of match %s."
+	return HttpResponse(response % matchselect_id)
+	
+def vote(request, matchselect_id):
+	return HttpResponse("You're voting on match %s." % matchselect_id)
