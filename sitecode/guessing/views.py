d2de8d9d6049f7b222e88f8391235a7bf480c4ed
Roosa <roosa_tammela@hotmail.com>
2014-12-02 19:25:30 +0000

    added render shortcuts and some 404 handling to views; added more test items into db

--- sitecode/guessing/views.py
+++ sitecode/guessing/views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 from guessing.models import Matchselect
 
 def index(request):
     latest_question_list = Matchselect.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('guessing/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'guessing/index.html', context)
 	
 def detail(request, matchselect_id):
-	return HttpResponse("Selected match: %s." % matchselect_id)
+	matchselect = get_object_or_404(Matchselect, pk=matchselect_id)
+	return render(request, 'guessing/detail.html', {'matchselect': matchselect})
 	
 def results(request, matchselect_id):
 	response = "Results of match %s."
