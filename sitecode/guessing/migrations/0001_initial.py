6d658ddcb746dac889fc659cc34e9556dbc24192
Roosa <roosa_tammela@hotmail.com>
2014-12-15 09:40:59 +0000

    added tracking of match results and user votes to database model

--- sitecode/guessing/migrations/0001_initial.py
+++ sitecode/guessing/migrations/0001_initial.py
@@ -2,21 +2,32 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Matchchoice',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('winner_choice', models.CharField(max_length=200)),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('winner_choice', models.CharField(max_length=50)),
                 ('votes', models.IntegerField(default=0)),
-                ('result', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Matchresult',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('winner', models.CharField(max_length=50)),
             ],
             options={
             },
@@ -25,13 +36,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Matchselect',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('match_info', models.CharField(max_length=200)),
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('game', models.CharField(max_length=200)),
+                ('team1', models.CharField(max_length=50)),
+                ('team2', models.CharField(max_length=50)),
                 ('match_date', models.DateTimeField(verbose_name='date of match')),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Uservotes',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('match', models.ForeignKey(to='guessing.Matchselect')),
+                ('voter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('winner_choice', models.ForeignKey(to='guessing.Matchchoice')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='matchresult',
+            name='match',
+            field=models.ForeignKey(to='guessing.Matchselect'),
+            preserve_default=True,
         ),
         migrations.AddField(
             model_name='matchchoice',
