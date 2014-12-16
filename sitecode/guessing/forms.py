7f177d428746994242dc61ccedc226db3e8f2261
Roosa <roosa_tammela@hotmail.com>
2014-12-16 20:54:17 +0000

    create_match now checks that match will happen in future

--- sitecode/guessing/forms.py
+++ sitecode/guessing/forms.py
@@ -2,7 +2,10 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
 from guessing.models import Matchselect, Matchchoice
-
+from django.core.exceptions import ValidationError
+import datetime
+from django.utils import timezone
+			
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
 
@@ -14,7 +17,16 @@ class MatchForm(forms.ModelForm):
 	game = forms.CharField(max_length=200)
 	team1 = forms.CharField(max_length=50)
 	team2 = forms.CharField(max_length=50)
-	match_date = forms.DateField(widget=SelectDateWidget)
+	# SelectDateWidget ensures year cannot be in past
+	match_date = forms.DateField(widget=SelectDateWidget, initial=timezone.now())
+
+	def clean_match_date(self):
+		date = self.cleaned_data['match_date']
+		t = timezone.now()
+		# Lengthy way of checking, but only way to compare datetime to date without errors
+		if ((date.year == t.year ) and (date.month < t.month)) or ((date.year == t.year) and (date.month == t.month) and (date.day < t.day)):
+			raise forms.ValidationError('The match date cannot be in the past.')
+		return date
 	
 	class Meta:
 		model = Matchselect
