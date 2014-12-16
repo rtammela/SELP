f2c09c578b8a1a989eeb362f134a5aadc207c599
Roosa <roosa_tammela@hotmail.com>
2014-12-16 22:05:18 +0000

    improved display of matches on admin site

--- sitecode/guessing/admin.py
+++ sitecode/guessing/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from guessing.models import Matchselect, Matchchoice, Matchresult
 
-class ResultInline(admin.StackedInline):
+class ResultInline(admin.TabularInline):
 	model = Matchresult
 	extra = 1
 	
 class MatchAdmin(admin.ModelAdmin):
+	list_display = ('game', 'match_date', 'team1', 'team2', 'creator')
+	list_filter = ['match_date']
+	fieldsets = [
+		(None,	{'fields': ['game', 'match_date']}),
+		('Teams', {'fields': ['team1', 'team2']})
+	]
 	inlines = [ResultInline]
 
 admin.site.register(Matchselect, MatchAdmin)
